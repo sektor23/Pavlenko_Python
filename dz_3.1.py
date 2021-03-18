9a47fde238a56eebcd76fe2f5a6b19983e76c9be
sektor23 <79736652+sektor23@users.noreply.github.com>
2021-03-18 11:31:49 +0300

    Add files via upload

---
+++ dz_3.1.py
@@ -0,0 +1,8 @@
+percent = int(input())
+if percent == 1:
+    print(percent, 'процент')
+elif percent > 1 and percent <= 4:
+    print(percent, 'процента')
+else:
+    percent >= 5 and percent <= 20
+    print(percent, 'процентов')
